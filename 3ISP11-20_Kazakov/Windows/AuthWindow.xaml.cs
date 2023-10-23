36adedecd6bc6354fe225fc92c8e810ce66ebc6a
Dimintin <kazakovdmitrit5073.word@gmail.com>
2023-10-23 18:15:12 +0300

    Добавьте файлы проекта.

---
+++ 3ISP11-20_Kazakov/Windows/AuthWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace _3ISP11_20_Kazakov.Windows
+{
+    /// <summary>
+    /// Логика взаимодействия для AuthWindow.xaml
+    /// </summary>
+    public partial class AuthWindow : Window
+    {
+        public AuthWindow()
+        {
+            InitializeComponent();
+
+            //Polygon p = new Polygon();
+            //p.Stroke = Brushes.Black;
+            //p.Fill = Brushes.DarkRed;
+            //p.StrokeThickness = 1;
+            //p.VerticalAlignment = VerticalAlignment.Bottom;
+            //p.Points = new PointCollection() { new Point(1, 100), new Point(560, 100), new Point(380, -230), new Point(0, -335) };
+            //GDMain.Children.Add(p);
+        }
+    }
+}
